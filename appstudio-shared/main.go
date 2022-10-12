5d2d97206c599be9973cbe1ac852fc7a30c66384
jparsai <jparsai@redhat.com>
2022-10-12 18:45:36 +0530

    Webhook-2

--- appstudio-shared/main.go
+++ appstudio-shared/main.go
@@ -89,18 +89,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err = (&appstudioredhatcomv1alpha1.ApplicationSnapshotEnvironmentBinding{}).SetupWebhookWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create webhook", "webhook", "ApplicationSnapshotEnvironmentBinding")
-		os.Exit(1)
-	}
-	if err = (&appstudioredhatcomv1alpha1.ApplicationSnapshot{}).SetupWebhookWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create webhook", "webhook", "ApplicationSnapshot")
-		os.Exit(1)
-	}
-	if err = (&appstudioredhatcomv1alpha1.ApplicationPromotionRun{}).SetupWebhookWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create webhook", "webhook", "ApplicationPromotionRun")
-		os.Exit(1)
-	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
